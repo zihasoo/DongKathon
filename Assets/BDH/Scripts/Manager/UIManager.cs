e3499c4d5e59ece9d31d250c15a809a6c3be901a
bbdh0531 <48090838+bbdh0531@users.noreply.github.com>
2025-07-31 15:14:14 +0900

    UI 리소스 변경 및 UI 리소스 추가
    ㅅㅂ 인 생 좃 같 네

--- Assets/BDH/Scripts/Manager/UIManager.cs
+++ Assets/BDH/Scripts/Manager/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,17 +12,26 @@ public class UIManager : MonoBehaviour
     public List<FoodOrderedUI> curOrdered = new List<FoodOrderedUI>();
     public List<FoodOrderedUI> completeOrdered = new List<FoodOrderedUI>();
 
-    public float padding = 0f;
-
     public GameObject foodOrderedObj;
 
     public Transform foodOrderOffPivot;
     public Transform foodOrderOnPivot;
 
+    public Vector2 foodOrderedDefualtSize = new Vector2(100f, 100f);
 
     public Vector2 foodOrderedFirstPov;
 
 
+    public TextMeshProUGUI ScoreText;
+
+
+    public Image inGameTimerGauge;
+    public TextMeshProUGUI inGameTimerText;
+
+    private void Initialized()
+    {
+        ScoreText.text = "0";
+    }
 
     void Start()
     {
@@ -30,7 +40,7 @@ public class UIManager : MonoBehaviour
 
     void CreateOrdered()
     {
-        for(int i = 0; i < GameManager.instance.MaxOrderedCount; i++)
+        for(int i = 0; i < GameManager.instance.maxOrderedCount; i++)
         {
             GameObject tmpOrdered = Instantiate(foodOrderedObj);
             FoodOrderedUI tmpfoodOdered = tmpOrdered.GetComponent<FoodOrderedUI>();
@@ -52,7 +62,7 @@ public class UIManager : MonoBehaviour
 
     public void GetOrder(Item item)
     {
-        if(GameManager.instance.CurOrderedCount >= GameManager.instance.MaxOrderedCount)
+        if(GameManager.instance.curOrderedCount >= GameManager.instance.maxOrderedCount)
         {
             Debug.Log("Ordered Pull");
             return;
@@ -71,36 +81,39 @@ public class UIManager : MonoBehaviour
         tmpOrdered.itemName = item.itemName;
         tmpOderedObj.transform.SetParent(foodOrderOnPivot);
         tmpOderedObj.SetActive(true);
+
         tmpRect.localScale = Vector3.one;
-        tmpRect.sizeDelta = new Vector2((float)item.uiSize, 100.0f);
-        
+        tmpRect.sizeDelta = new Vector2((float)item.uiSize, foodOrderedDefualtSize.y);
+        #region 배치 
+        //tmpRect.sizeDelta = new Vector2((float)item.uiSize, 100.0f);
+        ////주문이 있다면
+        //if (curOrdered.Count > 0)
+        //{
+        //    int index = GameManager.instance.curOrderedCount -1;
 
-        //주문이 있다면
-        if (curOrdered.Count > 0)
-        {
-            int index = GameManager.instance.CurOrderedCount -1;
+        //    //전에 음식주문 UI에 정보(위치 사이즈)를 가져온다
+        //    GameObject orderedObj = curOrdered[GameManager.instance.curOrderedCount-1].gameObject;
+        //    RectTransform orderedRTr = orderedObj.GetComponent<RectTransform>();
+        //    Vector3 prevPos = orderedRTr.localPosition;
+        //    float sizeDeltaX = orderedRTr.sizeDelta.x;
 
-            //전에 음식주문 UI에 정보(위치 사이즈)를 가져온다
-            GameObject orderedObj = curOrdered[GameManager.instance.CurOrderedCount-1].gameObject;
-            RectTransform orderedRTr = orderedObj.GetComponent<RectTransform>();
-            Vector3 prevPos = orderedRTr.localPosition;
-            float sizeDeltaX = orderedRTr.sizeDelta.x;
+        //    //전에 음식주문 UI정보(위치 사이즈)를 기반으로 다음 음식주문 UI에 x을 정한다 y, z는 동일한 값이다
+        //    tmpRect.localPosition = new Vector3(prevPos.x + padding + sizeDeltaX, foodOrderedFirstPov.y, 0.0f);
+        //}
+        //else // 주문이 없으면
+        //{
+        //    // 최조 위치시킨다
+        //    tmpRect.localPosition = new Vector3(foodOrderedFirstPov.x, foodOrderedFirstPov.y, 0.0f);
 
-            //전에 음식주문 UI정보(위치 사이즈)를 기반으로 다음 음식주문 UI에 x을 정한다 y, z는 동일한 값이다
-            tmpRect.localPosition = new Vector3(prevPos.x + padding + sizeDeltaX, foodOrderedFirstPov.y, 0.0f);
-        }
-        else // 주문이 없으면
-        {
-            // 최조 위치시킨다
-            tmpRect.localPosition = new Vector3(foodOrderedFirstPov.x, foodOrderedFirstPov.y, 0.0f);
-            
-            //tmpRect.localPosition = new Vector3(foodOrderedFirstPov.x, foodOrderedFirstPov.y, 0.0f);
-        }
+        //    //tmpRect.localPosition = new Vector3(foodOrderedFirstPov.x, foodOrderedFirstPov.y, 0.0f);
+        //}
+
+        #endregion
 
         //받아야할 주문 리스트 저장 완료한 리스트 제거
         curOrdered.Add(tmpOrdered);
         completeOrdered.Remove(completeOrdered[0]);
-        GameManager.instance.CurOrderedCount++;
+        GameManager.instance.curOrderedCount++;
 
 
         //음식로고 및 재료로고 예외처리
@@ -132,26 +145,36 @@ public class UIManager : MonoBehaviour
         }
 
         int index = curOrdered.FindIndex(o => o.itemName == item.itemName);
-
-        Debug.Log(index);
         
         FoodOrderedUI tmpFoodOrderUI = curOrdered[index];
-
         GameObject tmpUiObj = tmpFoodOrderUI.gameObject;
-        RectTransform tmpRectTr = tmpUiObj.GetComponent<RectTransform>();
-        Vector3 pos = tmpRectTr.localPosition;
 
+        // 비활성화 및 off 이동
         tmpUiObj.SetActive(false);
         tmpUiObj.transform.SetParent(foodOrderOffPivot);
-        tmpRectTr.localPosition = Vector3.zero;
 
-        tmpRectTr.sizeDelta = new Vector2(100f, 100f);
+        //초기화
+        tmpFoodOrderUI.itemName = "null";
+        tmpFoodOrderUI.foodLogo.sprite = null;
+        for(int i = 0; i < tmpFoodOrderUI.meterialArr.Length; i++)
+        {
+            tmpFoodOrderUI.meterialArr[i] = null;
+        }
+        tmpUiObj.GetComponent<RectTransform>().sizeDelta = new Vector2(foodOrderedDefualtSize.x, foodOrderedDefualtSize.y);
 
         curOrdered.Remove(tmpFoodOrderUI);
         completeOrdered.Add(tmpFoodOrderUI);
 
-        GameManager.instance.CurOrderedCount--;
+        GameManager.instance.curOrderedCount--;
     }
+
+    public void AddScore(int score)
+    {
+        GameManager.instance.inGameScore += score;
+        ScoreText.text = GameManager.instance.inGameScore.ToString();
+    }
+
+
 }
 
 
