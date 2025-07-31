2575e9cf4a0f06c1011a770a545a4acfa3ae54f9
zihasoo <dlquddbs-@daum.net>
2025-07-31 16:28:16 +0900

    자르기 UI 구현 완료 (인게임 구현 끝)

--- Assets/Scripts/CuttingTable.cs
+++ Assets/Scripts/CuttingTable.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CuttingTable : DefaultTable
 {
     private IEnumerator currentRoutine;
+    [SerializeField]
+    private Image gauge;
+    [SerializeField]
+    private GameObject canvas;
+
+    private void Start()
+    {
+        offGauge();
+    }
 
     public override bool putDownItem(ItemType givenFood)
     {
@@ -11,15 +21,16 @@ public class CuttingTable : DefaultTable
 
         currentItem = givenFood;
         var obj = Resources.Load<GameObject>($"Prefabs/{Table.itemTypeNameMap[currentItem]}");
-        Instantiate(obj, transform);
+        Instantiate(obj, pool);
         return true;
     }
 
     public void startCutting()
     {
-        if (currentItem == ItemType.None) return;
+        if (currentItem > ItemType.Cucumber) return;
         currentRoutine = cuttingCoroutine();
         StartCoroutine(currentRoutine);
+        canvas.SetActive(true);
     }
 
     public void stopCutting() 
@@ -27,24 +38,30 @@ public class CuttingTable : DefaultTable
         if (currentRoutine == null) return;
         StopCoroutine(currentRoutine);
         currentRoutine = null;
+        offGauge();
     }
 
     private void finishCutting()
     {
         currentItem = currentItem | (ItemType)isCutOff;
-        Destroy(transform.GetChild(0).gameObject);
+        Destroy(pool.GetChild(0).gameObject);
         var obj = Resources.Load<GameObject>($"Prefabs/{Table.itemTypeNameMap[currentItem]}");
-        Instantiate(obj, transform);
+        Instantiate(obj, pool);
+    }
+
+    private void offGauge()
+    {
+        gauge.fillAmount = 0;
+        canvas.SetActive(false);
     }
 
     private IEnumerator cuttingCoroutine()
     {
-        print("3");
-        yield return new WaitForSeconds(1);
-        print("2");
-        yield return new WaitForSeconds(1);
-        print("1");
-        yield return new WaitForSeconds(1);
+        for (int i = 1; i <= 10; i++)
+        {
+            gauge.fillAmount = 0.1f * i;
+            yield return new WaitForSeconds(0.2f);
+        }
         print("자르기 완료");
         finishCutting();
     }
--- Assets/Scripts/DefaultTable.cs
+++ Assets/Scripts/DefaultTable.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class DefaultTable : Table
 {
+    [SerializeField]
+    protected Transform pool;
     protected ItemType currentItem = ItemType.None;
 
     public override ItemType takeItem()
@@ -9,7 +11,7 @@ public class DefaultTable : Table
         if (currentItem == ItemType.None)
             return ItemType.None;
 
-        Destroy(transform.GetChild(0).gameObject);
+        Destroy(pool.GetChild(0).gameObject);
         var ret = currentItem;
         currentItem = ItemType.None;
         return ret;
@@ -22,7 +24,7 @@ public class DefaultTable : Table
         {
             currentItem = givenFood;
             var obj = Resources.Load<GameObject>($"Prefabs/{Table.itemTypeNameMap[currentItem]}");
-            Instantiate(obj, transform);
+            Instantiate(obj, pool);
             return true;
         }
         else
@@ -31,10 +33,10 @@ public class DefaultTable : Table
             if (((int)givenFood & isCutOff) != 0 && ((int)currentItem & isCutOff) != 0 && currentItem != givenFood)
                 //테이블에 있는 음식과 플레이어가 내려놓으려는 음식이 모두 잘린 음식이고 같은 음식이 아닐 때
             {
-                Destroy(transform.GetChild(0).gameObject); //원래 있던 음식 제거
+                Destroy(pool.GetChild(0).gameObject); //원래 있던 음식 제거
                 currentItem = ItemType.FinishedDish; //합쳐진(완성된) 음식으로 바꾸기
                 var obj = Resources.Load<GameObject>($"Prefabs/{Table.itemTypeNameMap[currentItem]}");
-                Instantiate(obj, transform);
+                Instantiate(obj, pool);
                 return true;
             }
             return false;
